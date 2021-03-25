8f9bfef7063c32e3ef570659e0df3b119ca2ffff
Crowdbotics <team@crowdbotics.com>
2021-03-25 16:04:08 +0000

    add package blueprint-react-native-camera-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Camera30218522', human_name: 'Camera 30', access_route: 'Camera30218522', icon: 'camera'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Camera30218522Navigator from '../features/Camera30218522/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Camera30218522: { screen: Camera30218522Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
