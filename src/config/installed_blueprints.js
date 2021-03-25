d592cab52613caf4c4301bca7ad5bac13b119ec3
Crowdbotics <team@crowdbotics.com>
2021-03-25 16:04:27 +0000

    add package blueprint-react-native-dashboard-1-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Dashboard122218530', human_name: 'Dashboard 1 22', access_route: 'Dashboard122218530'},
 { name: 'Settings23218529', human_name: 'Settings 23', access_route: 'Settings23218529'},
 { name: 'Dashboard229218523', human_name: 'Dashboard 2 29', access_route: 'Dashboard229218523'},
 { name: 'Camera30218522', human_name: 'Camera 30', access_route: 'Camera30218522', icon: 'camera'},
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import Dashboard122218530Reducer from '../features/Dashboard122218530/redux/reducers'
 import Dashboard229218523Reducer from '../features/Dashboard229218523/redux/reducers'
 
 export const combinedReducers = combineReducers({
@@ -16,6 +17,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+Dashboard122218530: Dashboard122218530Reducer,
 Dashboard229218523: Dashboard229218523Reducer,
 
 });
